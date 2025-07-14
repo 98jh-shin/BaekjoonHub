cf9dad5f9cf72a9bdbc18294cdac0859848fb4c7
98jh-shin <jhshin.dev@gmail.com>
2025-07-14 14:58:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -2,10 +2,7 @@ import sys
 
 num_list = [int(sys.stdin.readline().rstrip()) for _ in range(9)]
 
-sum_n = 0
-
-for n in num_list:
-    sum_n += n
+total = sum(num_list)
 
 find = False
 find_a = 0
@@ -15,16 +12,16 @@ find_b = 0
 for i in range(9):
     for j in range(i + 1, 9):
         temp = num_list[i] + num_list[j]
-        if sum_n - temp == 100:
-            find_a = num_list[i]
-            find_b = num_list[j]
+        if total - temp == 100:
+            find_a = i
+            find_b = j
             find = True
             break
     if find:
         break
 
-num_list.remove(find_a)
-num_list.remove(find_b)
+num_list.pop(find_b)
+num_list.pop(find_a)
 
 num_list.sort()
 print(*(i for i in num_list))
