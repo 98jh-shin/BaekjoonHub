f0909b55e2aab36a4a2f9c811201d53bfa2ceb74
98jh-shin <jhshin.dev@gmail.com>
2025-07-17 00:57:38 +0900

    [Gold V] Title: 하노이 탑, Time: 824 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,16 +1,13 @@
-s = int(input())
+n = int(input())
 
 def hanoi(n, start, end, temp):
-    if n > 20:
-        return
-    elif n == 1:
+    if n == 1:
         print(start, end)
-        return
-    elif n > 1:
+    if n > 1:
         hanoi(n - 1, start, temp, end)
         print(start, end)
         hanoi(n - 1, temp, end, start)
 
-print(2 ** s - 1)
-if s < 21:
-    hanoi(s, 1, 3, 2)
+print(2 ** n - 1)
+if n < 21:
+    hanoi(n, 1, 3, 2)
