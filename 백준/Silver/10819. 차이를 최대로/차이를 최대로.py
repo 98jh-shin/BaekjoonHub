a03f16c04476e99a2392dd96c57c09e8125ca5a0
98jh-shin <jhshin.dev@gmail.com>
2025-07-16 22:49:26 +0900

    [Silver II] Title: 차이를 최대로, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,29 +1,13 @@
 import sys
+from itertools import permutations
 
 n = int(input())
 nums = list(map(int, sys.stdin.readline().split()))
-max_total = 0
-path = []
-visited = [False] * n
 
+result = 0
 
-def dfs(depth):
-    global max_total
-    if depth == n:
-        temp = 0
-        for i in range(n - 1):
-            temp += abs(path[i] - path[i + 1])
-        if max_total < temp:
-            max_total = temp
-        return
+for perm in permutations(nums):
+    temp = sum(abs(perm[i] - perm[i + 1]) for i in range(n - 1))
+    result = max(result, temp)
 
-    for i in range(n):
-        if not visited[i]:
-            visited[i] = True
-            path.append(nums[i])
-            dfs(depth + 1)
-            path.pop()
-            visited[i] = False
-    return
-dfs(0)
-print(max_total)
+print(result)
