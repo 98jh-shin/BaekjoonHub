98a993730293df6c65385c9183b3c7dd25a7b6e9
98jh-shin <jhshin.dev@gmail.com>
2025-07-19 00:33:10 +0900

    [Gold V] Title: 괄호의 값, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,62 +1,49 @@
 import sys
 
-input = sys.stdin.readline().strip()
-arr = []
-for ch in input:
+def solve():
+    s = sys.stdin.readline().rstrip()
+    stack = []
 
-    if ch == '(':
-        arr.append(ch)
-    elif ch == '[':
-        arr.append(ch)
-    elif ch == ')':
-        if not arr:
-            print(0)
-            exit()
-        prev = arr.pop()
-        tmp = 0
-        while prev.isdigit():
-            tmp += int(prev)
-            if not arr:
+    for c in s:
+        if c in '([':
+            stack.append(c)
+
+        elif c in ')]':
+            # 1) 빈 스택 체크
+            if not stack:
                 print(0)
-                exit()
-            prev = arr.pop()
+                return
 
-        if prev == '(':
-            if tmp == 0:
-                arr.append("2")
-            else:
-                arr.append(str(tmp * 2))
-        else:
-            print(0)
-            exit()
-    elif ch == ']':
-        if not arr:
-            print(0)
-            exit()
-        prev = arr.pop()
-        tmp = 0
-        while prev.isdigit():
-            tmp += int(prev)
-            if not arr:
+            # 2) 숫자 문자열 합산
+            tmp = 0
+            while stack and stack[-1].isdigit():
+                tmp += int(stack.pop())
+
+            # 3) 짝 여는 괄호·배수 결정
+            pair = '(' if c == ')' else '['
+            mul  = 2   if c == ')' else 3
+            if not stack or stack[-1] != pair:
                 print(0)
-                exit()
-            prev = arr.pop()
+                return
 
-        if prev == '[':
-            if tmp == 0:
-                arr.append("3")
-            else:
-                arr.append(str(tmp * 3))
+            # 4) 여는 괄호 제거 후 결과 푸시
+            stack.pop()
+            stack.append(str(tmp * mul if tmp else mul))
+
         else:
+            # 허용되지 않는 문자
             print(0)
-            exit()
-    else:
-        print(0)
-        exit()
-res = 0
-for i in arr:
-    if i == '(' or i == '[' or i == ')' or i == ']':
-        print(0)
-        exit()
-    res += int(i)
-print(res)
+            return
+
+    # 남은 게 모두 숫자여야 최종 합산
+    result = 0
+    for x in stack:
+        if not x.isdigit():
+            print(0)
+            return
+        result += int(x)
+
+    print(result)
+
+if __name__ == "__main__":
+    solve()
