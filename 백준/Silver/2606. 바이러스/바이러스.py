bf4a71f7eac871bbd2da4b6bc9567cc6abe6cb0f
98jh-shin <jhshin.dev@gmail.com>
2025-07-25 13:36:54 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+con = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    con[i].append(j)
+    con[j].append(i)
+
+visited = [False] * (n + 1)
+
+count = 0
+def dfs(x):
+    visited[x] = True
+    global count
+    count += 1
+    for com in con[x]:
+        if not visited[com]:
+            dfs(com)
+
+
+dfs(1)
+print(count - 1)
