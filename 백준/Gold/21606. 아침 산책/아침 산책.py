df77f9ca74d6cff46464b12d7f66153dfb59861d
98jh-shin <jhshin.dev@gmail.com>
2025-07-28 15:22:11 +0900

    [Gold III] Title: 아침 산책, Time: 1000 ms, Memory: 109036 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -4,11 +4,14 @@ n = int(sys.stdin.readline().rstrip())
 
 inout = list(map(int, sys.stdin.readline().rstrip()))
 graph = [[] for _ in range(n + 1)]
-
+res = 0
 for i in range(n - 1):
     a, b = map(int, sys.stdin.readline().rstrip().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    if inout[a - 1] == 1 and inout[b - 1] == 1:
+        res += 2
+    else:
+        graph[a].append(b)
+        graph[b].append(a)
 
 def dfs(cur, visited):
     visited[cur] = True
@@ -21,10 +24,11 @@ def dfs(cur, visited):
                 count = count + dfs(nxt, visited)
     return count
 
-res = 0
+visited = [False] * (n + 1)
+
 for i in range(1, n + 1):
-    visited = [False] * (n + 1)
-    if inout[i - 1] == 1:
-        res += dfs(i, visited)
+    if inout[i - 1] == 0 and not visited[i]:
+        count = dfs(i, visited)
+        res += count * (count - 1)
 
 print(res)
