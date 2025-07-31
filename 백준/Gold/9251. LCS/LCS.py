d234cba95bb74d323daeab46559870e6692642c2
98jh-shin <jhshin.dev@gmail.com>
2025-07-31 23:11:17 +0900

    [Gold V] Title: LCS, Time: 216 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,17 +1,17 @@
-import sys
+X = input()
+Y = input()
 
-a = sys.stdin.readline().strip()
-b = sys.stdin.readline().strip()
+def LCS(X, Y):
+    n, m = len(X), len(Y)
+    memo = [[0] * (m + 1) for _ in range(n + 1)]
 
-dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+    for i in range(n):
+        for j in range(m):
+            if X[i] == Y[j]:
+                memo[i+1][j+1] = memo[i][j] + 1
+            else:
+                memo[i+1][j+1] = max(memo[i][j+1], memo[i+1][j])
+    return memo[n][m]
 
-for i in range(1, len(a) + 1):
-    for j in range(1, len(b) + 1):
-        if a[i - 1] == b[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-
-
-print(dp[len(a)][len(b)])
+result = LCS(X, Y)
+print(result)
