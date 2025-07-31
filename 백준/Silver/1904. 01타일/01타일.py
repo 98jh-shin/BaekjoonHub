729192a622cd9f127fe4a656fe13259e7be30600
98jh-shin <jhshin.dev@gmail.com>
2025-07-31 17:47:02 +0900

    [Silver III] Title: 01타일, Time: 188 ms, Memory: 52700 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -4,12 +4,15 @@ if n < 3:
     print(n)
     exit()
 
-dp_1 = 1 # dp 배열에 보관 x 변수에 저장
-dp_2 = 2
-for _ in range(3, n + 1):
-    tmp = dp_1
-    dp_1 = dp_2
-    dp_2 = (tmp + dp_1) % 15746
+length = (n // 2) + 1
+dp = [0] * (length + 2)
 
-print(dp_2)
+dp[0] = 1
+dp[1] = 1
 
+for i in range(2, length + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+result = (dp[length] * dp[(n - 1) - (length - 1)]) + (dp[length - 1] * dp[(n - 1) - length])
+
+print(result % 15746)
