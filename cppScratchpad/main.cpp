6ef6d48f9fddd2cf8c3bc64517ad1f0789d2528f
shinjungho <efsxr879@gmail.com>
2025-08-13 10:25:14 +0900

    test

--- cppScratchpad/main.cpp
+++ cppScratchpad/main.cpp
@@ -1,40 +1,73 @@
 #include <iostream>
-#include <stack>
-#include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
+
+vector<int> graph[501];
+vector<bool> visited;
+
+bool bfs(int start) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    int node_cnt = 0;
+    int edge_cnt = 0;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        node_cnt++;
+        for (int next : graph[cur]) {
+            edge_cnt++;
+            if (!visited[next]) {
+                q.push(next);
+                visited[next] = true;
+            }
+        }
+    }
+    return (node_cnt - 1) == (edge_cnt / 2);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
+    int test_count = 1;
+    while (true) {
+        int n, m;
+        cin >> n >> m;
+        if (n == 0 && m == 0) break;
 
-    string str;
-    stack<int> s;
-    int index = 1;
-    int i = 0;
-    int x;
+        for (int i = 1; i < n + 1; i++) {
+            graph[i].clear();
+        }
+        visited.assign(n + 1, false);
 
-    while (i < n) {
-        cin >> x;
-
-        while (x >= index) {
-            str += "+\n";
-            s.push(index++);
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a].push_back(b);
+            graph[b].push_back(a);
         }
 
-        if (s.top() == x) {
-            str += "-\n";
-            s.pop();
-            ++i;
+        int tree_cnt = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (!visited[i]) {
+                tree_cnt += bfs(i);
+            }
+        }
+
+        if (tree_cnt == 0) {
+            cout << "Case " << test_count << ": No trees.\n";
+        } else if (tree_cnt == 1) {
+            cout << "Case " << test_count << ": There is one tree.\n";
         } else {
-            cout << "NO\n";
-            return 0;
+            cout << "Case " << test_count << ": A forest of " << tree_cnt << " trees.\n";
         }
+        test_count++;
     }
-
-    cout << str;
     return 0;
 }
