df31ddf8661181423b21e94cc05a724833154aa0
shinjungho <efsxr879@gmail.com>
2025-08-13 10:22:38 +0900

    알고리즘 스터디 발표용 코드 복사

---
+++ cppScratchpad/week05al/1874.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    string str;
+    stack<int> s;
+    int index = 1;
+    int i = 0;
+    int x;
+
+    while (i < n) {
+        cin >> x;
+
+        while (x >= index) {
+            str += "+\n";
+            s.push(index++);
+        }
+
+        if (s.top() == x) {
+            str += "-\n";
+            s.pop();
+            ++i;
+        } else {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+
+    cout << str;
+    return 0;
+}
