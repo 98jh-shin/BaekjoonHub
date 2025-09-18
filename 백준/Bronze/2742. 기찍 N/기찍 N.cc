75bcb0367e9cfe510fd77552d65c5cdfcc2d4da4
98jh-shin <jhshin.dev@gmail.com>
2025-09-18 10:49:47 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    for (int i = t; i > 0; --i) {
+        cout << i << '\n';
+    }
+}
