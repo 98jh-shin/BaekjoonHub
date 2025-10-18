bc033f55db7ce533d9180cd3d1b45aad49401d4a
98jh-shin <jhshin.dev@gmail.com>
2025-10-18 20:54:58 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,19 @@
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+
+cards = list(map(int, sys.stdin.readline().split()))
+
+total = 0
+
+max_sum = 0
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            card_sum = cards[i] + cards[j] + cards[k]
+
+            if card_sum <= m:
+                max_sum = max(max_sum, card_sum)
+
+print(max_sum)
