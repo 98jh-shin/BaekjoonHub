c5a51f7134667b30e6b32a65086ae10db6603c6a
98jh-shin <jhshin.dev@gmail.com>
2026-01-05 16:30:46 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        Double x = sc.nextDouble();
+        Double y = sc.nextDouble();
+        
+        System.out.print(x / y);
+        
+        sc.close();
+    }
+}
